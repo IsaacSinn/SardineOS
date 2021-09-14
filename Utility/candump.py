b7f52b435212501e76f55329022bee6c80530fff
DeadlyCucumber06 <78033807+DeadlyCucumber06@users.noreply.github.com>
2021-09-14 19:21:22 +0800

    Update candump.py
    Display both hex and int, supports more than three bytes of data

--- Utility/candump.py
+++ Utility/candump.py
@@ -5,4 +5,12 @@ bus = at_serial_can.ATSerialBus(channel=sys.argv[1], bitrate=250000)
 
 while True:
     msg = bus.recv()
-    print(f'From: 0x{msg.arbitration_id:02X}, {msg}')
+    #print(f'From: 0x{msg.arbitration_id:02X} Data: {msg.data[0]:02X} {msg.data[1]:02X} {msg.data[2]:02X}')
+    if msg:
+        print(f'From: 0x{msg.arbitration_id:02X} Hexadecimal:', end=" ")
+        for i in range(msg.dlc):
+            print(f'{msg.data[i]:02X}', end=" ")
+        print(f'Integer:', end=" ")
+        for i in range(msg.dlc):
+            print(f'{int(msg.data[i])}', end=" ")
+        print(end=" \n")
